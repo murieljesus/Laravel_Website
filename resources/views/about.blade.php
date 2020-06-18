3f44699a5c7486c922434ffcfcbf8f353cc68074
Jesus <jmuriel72@gmail.com>
2020-06-17 23:15:31 -0300

    about last

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,18 +2,24 @@
 <title>Nosotros</title>
 @section('contenido')
 
-<div class="container mx-auto col-md-5">
+<div class="container mx-auto col-md-6">
     <div class="row">
-            <img  class="img-fluid" src="img/react.svg" alt="">
+        <img  class="img-fluid" src="img/hydro2.png" alt="">
+        <div class="container-fluid ">
             <div class="card-body texto">
              <h1>Quienes somos</h1>
-             <p class="lead text-secondary">
-                 nos desarrollamos dia a dia 
-                 con el compromiso de la gente 
-                 para un mejor servicio
-                 de mayor calidad 
-            </p>
-            </div>
+                <p class="lead text-secondary text-justify "> 
+                 Hydro es una asociación civil, sin fines de lucro, creada mediante el Decreto 1192/92 del 10 de julio de 1992, que se constituyó el 29 de julio de 1992 y nuclea a la mayor parte de las empresas generadoras de electricidad del país.
+                 </p>
+                 <p class="lead text-secondary text-justify ">De la misma manera que la Asociación de Distribuidores de Energía Eléctrica de la República Argentina (ADEERA), la Asociación de Grandes Usuarios de Energía Eléctrica de la República Argentina (AGUEERA), la Asociación de Transportistas de Energía Eléctrica de la República Argentina (ATEERA) y el Estado Nacional.
+                     </p> 
+                     <p class="lead text-secondary text-justify "> Hydro propicia el adecuado funcionamiento y la transparencia del mercado eléctrico, con señales de precio y seguridad jurídica que aseguren el abastecimiento eléctrico, garantizando a todos los usuarios del país, en el corto y largo plazo, un servicio de calidad, mejorando dia a dia.</p>
+               
+                
+             </div>
+        </div>
+            
+         </div>
     </div>
 </div>
 @endsection
