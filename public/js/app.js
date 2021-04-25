bbf7d00ed3fac6ba55f08c9af33849f3ce24c896
murieljesus <jmuriel72@gmail.com>
2021-04-25 02:57:58 -0300

    'update'

--- public/js/app.js
+++ public/js/app.js
@@ -50347,9 +50347,9 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\laragon\www\app\resources\js\app.js */"./resources/js/app.js");
-__webpack_require__(/*! C:\laragon\www\app\resources\sass\app.scss */"./resources/sass/app.scss");
-module.exports = __webpack_require__(/*! C:\laragon\www\app\resources\sass\toastr.scss */"./resources/sass/toastr.scss");
+__webpack_require__(/*! D:\laragon\www\app\resources\js\app.js */"./resources/js/app.js");
+__webpack_require__(/*! D:\laragon\www\app\resources\sass\app.scss */"./resources/sass/app.scss");
+module.exports = __webpack_require__(/*! D:\laragon\www\app\resources\sass\toastr.scss */"./resources/sass/toastr.scss");
 
 
 /***/ })
